ad17fdbaedbb029c2d8ad26407dbbcec7c927e04
Ayush2004sharma <ayush.sharma.cseds.2022@miet.iac.in>
2025-10-14 19:49:36 +0530

    commit

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://medsync-backend-oyxb.onrender.com",
   withCredentials: true, // optional if backend uses cookies
 });
 
